715c51826466a5d43d9700fed4f7dfb5e76dcf17
klolapc\klola <ariantony@gmail.com>
2017-04-29 14:44:29 +0700

    Counter Number

--- app/src/main/java/com/example/klola/projecthappybirthday/MainActivity.java
+++ app/src/main/java/com/example/klola/projecthappybirthday/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.klola.projecthappybirthday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hal2);
+
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score++;
+                TextView text = (TextView) findViewById(R.id.clickNumber);
+                text.setText(Integer.toString(score));
+            }
+        });
     }
+
 }
