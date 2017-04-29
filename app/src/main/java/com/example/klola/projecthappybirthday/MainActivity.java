6528494b91f049b161d4fc54862820f93603d64e
klolapc\klola <ariantony@gmail.com>
2017-04-29 15:45:37 +0700

    Penjumlahan dari input text

--- app/src/main/java/com/example/klola/projecthappybirthday/MainActivity.java
+++ app/src/main/java/com/example/klola/projecthappybirthday/MainActivity.java
@@ -4,26 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
+    EditText et1, et2;
+    TextView cont_jababan;
+    Button btnhitung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hal2);
+        setContentView(R.layout.activity_kalkulator);
 
-        Button btn = (Button) findViewById(R.id.button1);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                score++;
-                TextView text = (TextView) findViewById(R.id.clickNumber);
-                text.setText(Integer.toString(score));
-            }
-        });
+        et1 = (EditText) findViewById(R.id.angka1);
+        et2 = (EditText) findViewById(R.id.angka2);
+        cont_jababan = (TextView) findViewById(R.id.cont_jabawan);
+        btnhitung = (Button) findViewById(R.id.hitungAngka);
+
+btnhitung.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if(!et1.getText().toString().equals("") && !et2.getText().toString().equals("")){
+            int a1 = Integer.parseInt(et1.getText().toString());
+            int a2 = Integer.parseInt(et2.getText().toString());
+            int jawaban = a1 + a2;
+            cont_jababan.setText(Integer.toString(jawaban));
+        }else{
+            cont_jababan.setText("anda belum mengisi angka");
+        }
     }
+});
 
+/*
+* Edit text >
+* */
+    }
 }
